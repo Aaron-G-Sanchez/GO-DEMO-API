51dce0baf7885185ffdcd2f07ca641001b9fbbd8
Aaron Sanchez <saarong12@gmail.com>
2023-11-14 00:12:44 -0600

    Removed unused albums array

--- main.go
+++ main.go
@@ -7,34 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// type album struct {
-// 	ID     string  `json:"id"`
-// 	Title  string  `json:"title"`
-// 	Artist string  `json:"artist"`
-// 	Price  float64 `json:"price"`
-// }
-
-// var albums = []album{
-// 	{
-// 		ID:     "1",
-// 		Title:  "Torches",
-// 		Artist: "Foster the People",
-// 		Price:  10.99,
-// 	},
-// 	{
-// 		ID:     "2",
-// 		Title:  "SMITHEREENS",
-// 		Artist: "Joji",
-// 		Price:  24.99,
-// 	},
-// 	{
-// 		ID:     "3",
-// 		Title:  "Dark Sun",
-// 		Artist: "Day Seeker",
-// 		Price:  15.99,
-// 	},
-// }
-
 func init() {
 	initializers.LoadEnvVariables()
 }
