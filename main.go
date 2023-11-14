6316a61259d03b36070a26e7b979cf5c94d25b52
Aaron Sanchez <saarong12@gmail.com>
2023-11-13 18:34:15 -0600

    Created a basic API in Go using Gin, Godotenv, and Compile Deamon

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/aaron-g-sanchez/go-crud/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{
+		ID:     "1",
+		Title:  "Torches",
+		Artist: "Foster the People",
+		Price:  10.99,
+	},
+	{
+		ID:     "2",
+		Title:  "SMITHEREENS",
+		Artist: "Joji",
+		Price:  24.99,
+	},
+	{
+		ID:     "3",
+		Title:  "Dark Sun",
+		Artist: "Day Seeker",
+		Price:  15.99,
+	},
+}
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/albums", getAlbums)
+
+	router.Run()
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(200, albums)
+}
